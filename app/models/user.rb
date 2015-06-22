8cfe3389932aaff49eb3f4f3a9faec5392d0b7b3
Ben Barber <contact@benbarber.co.uk>
2015-06-22 21:06:30 +0100

    Create token signature for any resource

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # app/models/user.rb
 class User < ActiveRecord::Base
   has_secure_password
-  before_create :generate_signature
+  before_create generate_signature: :auth
 
   EMAIL_FORMAT = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 
@@ -14,17 +14,18 @@ class User < ActiveRecord::Base
                                     if: :password_digest_changed?
 
   def invalidate_auth_tokens
-    generate_signature
+    generate_signature :auth
     save
   end
 
   private
 
-  def generate_signature
+  def generate_signature(resource)
     loop do
-      self.auth_signature = SecureRandom.urlsafe_base64
-      self.auth_signature_created_at = Time.now
-      break unless User.find_by(auth_signature: auth_signature)
+      send "#{resource}_signature=", SecureRandom.urlsafe_base64
+      send "#{resource}_signature_created_at=", Time.zone.now
+      break unless User.find_by("#{resource}_signature",
+                                send("#{resource}_signature"))
     end
   end
 end
